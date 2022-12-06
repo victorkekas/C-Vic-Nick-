1cdc9d449b239dc861ac5e2a51c78c173d752f2e
victorkekas <vikor7007@gmail.com>
2022-12-06 14:13:59 +0200

    Libs_Linked

--- src/Movie/Movie/widgets.h
+++ src/Movie/Movie/widgets.h
@@ -7,6 +7,7 @@ using namespace std;
 class Widget {
 
 public :
+
 	vector <int> position;	// Thesi pano ston kamva 
 	bool can_move= false ;	// an mporei na paei dexia aristera 
 	bool can_press= false ; // an mporei na patithei 
