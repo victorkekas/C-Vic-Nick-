03d9881ac0cb73ed30bc744eed66208b78663f0c
Nikolaos-Vaitsis <vnikolas21@gmail.com>
2022-12-11 03:11:00 +0200

    aaaa

---
+++ src/Movie/Movie/container_box.cpp
@@ -0,0 +1,21 @@
+#include "container_box.h"
+
+ContainerBox::ContainerBox(float center_x, float center_y, float width, float height)
+{
+	this->side_00 = center_x - (width / 2);
+	this->side_x0 = center_x + (width / 2);
+	this->side_0y = center_y - (height / 2);
+	this->side_xy = center_y + (height / 2);
+}
+
+ContainerBox::~ContainerBox()
+{
+}
+
+bool ContainerBox::contained(float mouse_pos_x, float mouse_pos_y)
+{
+	if (side_00 <= mouse_pos_x<=side_x0 && side_0y<=mouse_pos_y <= side_xy) {
+		return true;
+	}
+	return false;
+}
---
+++ src/Movie/Movie/container_box.h
@@ -0,0 +1,9 @@
+#pragma once
+class ContainerBox {
+private:
+	float side_00, side_x0, side_0y, side_xy;
+public:
+	ContainerBox(float center_x, float center_y, float width, float height); // give as variables the coordinates of an button (rectangle)
+	~ContainerBox();
+	bool contained(float mouse_pos_x, float mouse_pos_y/*dedomena tou mouse position*/); //return true if mouse is in the container box
+};
